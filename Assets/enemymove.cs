2c6bf06b02115ec009be2f33eafe7deb9cd192f1
joeleman <90899225+joeleman@users.noreply.github.com>
2021-09-27 16:29:44 +0100

    duckupdated

--- Assets/enemymove.cs
+++ Assets/enemymove.cs
@@ -1,20 +1,52 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class enemymove : MonoBehaviour
 {
+
     public GameObject player;
-    public float enemy; 
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        float ex = transform.position.x;
+
+        float px = player.transform.position.x;
+
+
+
+
+        void DoFaceLeft(bool faceleft)
+        {
+            if (faceleft == true)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+
+
+        if (ex < px)
+        {
+            DoFaceLeft(true);
+        }
+        else
+        {
+            DoFaceLeft(false);
+        }
+
+
+
     }
+
 }
-    */
